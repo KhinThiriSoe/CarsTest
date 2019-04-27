0892df5a3dbe5abcfdbe79fe903ddc6f14682ba4
Khin <khinthirisoe.mdy@gmail.com>
2019-04-27 15:18:53 +0700

    remove unnecessary code

--- app/src/main/java/com/sevenpeakssoftware/khinthirisoe/ui/base/BaseViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.sevenpeakssoftware.khinthirisoe.ui.base
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-abstract class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
